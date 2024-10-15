d0383e5adc33d6231bc5e20d36d565d6249e4051
Luka Udovicic <lukaudovicic10@gmail.com>
2024-10-15 14:23:35 +0200

    some tweaks

--- Assets/Scripts/Follow.cs
+++ Assets/Scripts/Follow.cs
@@ -1,18 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Follow : MonoBehaviour
 {
     public Transform target;
-    public UnityEngine.AI.NavMeshAgent navMeshAgent;
-        // Update is called once per frame
-        void Start()
-        {
-        }
+    public NavMeshAgent navMeshAgent;
+
     void Update()
     {
-        navMeshAgent.updatePosition = false;
-        navMeshAgent.nextPosition = target.position;
+        navMeshAgent.SetDestination(target.position);
     }
 }
